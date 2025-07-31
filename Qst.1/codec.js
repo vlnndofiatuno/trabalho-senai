4c3c7c77010e940bea268f21552abaa2f712f8f0
vlnndofiatuno <pedropcvalentin@gmail.com>
2025-07-31 19:16:29 -0300

    pedrinho

---
+++ Qst.1/codec.js
@@ -0,0 +1,6 @@
+const prompt = require("prompt-sync")();
+
+let numero1 = Number(prompt("Digite um número: "));
+let numero2 = Number(prompt("Digite mais um número: "));
+
+console.log("Soma:", numero1 + numero2);
